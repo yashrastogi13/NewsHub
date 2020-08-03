e377a7d5d6d0f24bfc25dd16f40825bbe7d9e15a
yashrastogi <yashrastogi.15082@gmail.com>
2020-08-04 03:20:25 +0530

    added Procfile and installed gunicorn

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f=%t#*jie7$5naxk(8-&f1dtac+3mi65-m4xgqdmkldy@nnov9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
